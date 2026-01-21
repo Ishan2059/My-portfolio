8a4df187f9a6adfad147eda2d9b4249247239585
Ishan Mishra <88926220+Ishan2059@users.noreply.github.com>
2026-01-21 22:29:08 +0545

    Wavy Effect
    New wavy background effect added

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,36 @@
+"use client";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { EtheralShadow } from "./components/ui/etheral-shadow";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-white dark:bg-black font-sans selection:bg-indigo-500 selection:text-white">
-      <Navbar />
-      <main className="flex-grow">
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
+    <div className="relative flex flex-col min-h-screen bg-white dark:bg-black font-sans selection:bg-indigo-500 selection:text-white">
+      {/* Fixed Ethereal Shadow Background */}
+      <div className="fixed inset-0 z-0 opacity-50 pointer-events-none">
+        <EtheralShadow
+          animation={{ scale: 60, speed: 70 }}
+          noise={{ opacity: 0.4, scale: 1.2 }}
+          sizing="fill"
+        />
+      </div>
+
+      {/* Content Layer */}
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Hero />
+          <About />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
